ecbc7b399d1232b8124485640814039aae32b693
Neal <kb5706@gmail.com>
2018-12-17 13:42:10 +0800

    添加起始点合格性验证和坐标运算中的撞墙验证

--- trace.py
+++ trace.py
@@ -8,7 +8,7 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
         self._content = ""
         self._tag = ""
 
-    def startElement(self,name,attrs):
+    def startElement(self,name,attrs): #读取开始标签
         self._tag = name
         if name == "trace":
             print("begin trace")
@@ -17,53 +17,97 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
         if name == "step":
             print("begin step")
 
-    def endElement(self,name):
+    def endElement(self,name):          #读取结束标签和标签内的内容
         if name == "trace":
             print("end trace")
-        if name == "beginspot":
+        if name == "beginspot":                     #读取起始坐标，给起始坐标marked green
             print("beginspot is "+self._content)
             coordinate = self._content.split(",")
             print(coordinate)
-            self.x = coordinate[0]
-            self.y = coordinate[1]
-            print("x is " + self.x)
-            print("y is " + self.y)
-            print("make the original point green")
+            self.x = int(coordinate[0])
+            self.y = int(coordinate[1])
+            print("x is " + str(self.x))
+            print("y is " + str(self.y))
+            if self.x >= 0 and self.x <=30 and self.y >= 0 and self.y <= 30:
+                print("make the original point green")
+            else:
+                print("请设定起始坐标在30*30的范围内")
         if name == "step":
-            print("step is "+self._content)
-            # #以下区块是判断移动方向，并按照相应方向进行坐标计算，但是运行报错，所以暂且注释##
-            # if self._content == "up":
-            #     self.y = self.y  +1
-            # if self._content == "down":
-            #     self.y = self.y - 1
-            # if self._content == "left":
-            #     self.x = self.x -1
-            # if self._content == "right":
-            #     self.x = self.x +1
-            # if self._content == "leftup":
-            #     self.x = self.x -1
-            #     self.y = self.y +1
-            # if self._content == "leftdown":
-            #     self.y = self.y -1
-            #     self.x = self.x -1
-            # if self._content == "rightup":
-            #     self.y = self.y +1
-            #     self.x = self.x +1
-            # if self._content == "rightdown":
-            #     self.y = self.y -1
-            #     self.x = self.x +1
+            print("step is "+self._content) #判断移动方向，并按照相应方向进行坐标计算
+            if self._content == "up":
+                self.y = self.y +1
+                if self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+
+            if self._content == "down":
+                self.y = self.y - 1
+                if self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "left":
+                self.x = self.x -1
+                if self.x >= 0 and self.x <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "right":
+                self.x = self.x +1
+                if self.x >= 0 and self.x <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "leftup":
+                self.x = self.x -1
+                self.y = self.y +1
+                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "leftdown":
+                self.y = self.y -1
+                self.x = self.x -1
+                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "rightup":
+                self.y = self.y +1
+                self.x = self.x +1
+                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
+            if self._content == "rightdown":
+                self.y = self.y -1
+                self.x = self.x +1
+                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                    print("x is " + str(self.x))
+                    print("y is " + str(self.y))
+                else:
+                    print("hit wall")
+                    print("mark the point red")
             # ###########################################
     def characters(self,content):
         self._content = content
 
-    def hitwall(self,x,y):#自定义的撞墙方法，用于标注行动轨迹超出30*30范围
-        self.x = x
-        self.y = y
-        if x>30 or x<0 or y>30 or y< 0:
-            print("hit wall")
-            print("make the point red")
-
 if __name__ == "__main__":
     handler = MoveHandler()
-    sax.parse("eightDirctions.xml",handler)
-    #sax.parse("testHitWall.xml", handler)
+    #sax.parse("eightDirctions.xml",handler)
+    sax.parse("testHitWall.xml", handler)
