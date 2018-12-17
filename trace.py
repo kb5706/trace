15a2496fe2713291b59dfa2ad4239f1193cd7937
Neal <kb5706@gmail.com>
2018-12-17 14:27:49 +0800

    添加轨迹画笔，运行出错的初版

--- trace.py
+++ trace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from xml import sax
+import turtle
 
 class MoveHandler(sax.ContentHandler): #继承ContentHandler类
     def __init__(self):
@@ -12,6 +13,10 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
         self._tag = name
         if name == "trace":
             print("begin trace")
+            print("设定初始值")          #初始化绘制基本参数
+            t = turtle.Pen()
+            t.pensize(5)
+            t.pencolor("blue")
         if name == "beginspot":
             print("begin beginspot")
         if name == "step":
@@ -20,7 +25,8 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
     def endElement(self,name):          #读取结束标签和标签内的内容
         if name == "trace":
             print("end trace")
-        if name == "beginspot":                     #读取起始坐标，给起始坐标marked green
+            t.done()
+        if name == "beginspot":                     #读取起始坐标
             print("beginspot is "+self._content)
             coordinate = self._content.split(",")
             print(coordinate)
@@ -28,81 +34,108 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
             self.y = int(coordinate[1])
             print("x is " + str(self.x))
             print("y is " + str(self.y))
-            if self.x >= 0 and self.x <=30 and self.y >= 0 and self.y <= 30:
-                print("make the original point green")
+            t.penup()
+            t.goto(self.x, self.y)
+            t.pencolor("green")
+            t.pendown()
+            if self.x >= 0 and self.x <=30 and self.y >= 0 and self.y <= 30:    #判断起始坐标是否在30*30范围
+                print("ready to trace")
             else:
-                print("请设定起始坐标在30*30的范围内")
-        if name == "step":
-            print("step is "+self._content) #判断移动方向，并按照相应方向进行坐标计算
+                print("请设定起始坐标在30*30的范围内")###########如果不在范围内程序可能仍会运行#
+        if name == "step":                                  #判断移动方向，并按照相应方向进行坐标计算
+            print("step is "+self._content)                 #超出30*30就会撞墙
             if self._content == "up":
                 self.y = self.y +1
                 if self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x,self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
 
             if self._content == "down":
                 self.y = self.y - 1
                 if self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "left":
                 self.x = self.x -1
                 if self.x >= 0 and self.x <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "right":
                 self.x = self.x +1
                 if self.x >= 0 and self.x <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "leftup":
                 self.x = self.x -1
                 self.y = self.y +1
                 if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "leftdown":
                 self.y = self.y -1
                 self.x = self.x -1
                 if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "rightup":
                 self.y = self.y +1
                 self.x = self.x +1
                 if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             if self._content == "rightdown":
                 self.y = self.y -1
                 self.x = self.x +1
                 if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
+                    t.goto(self.x, self.y)
                 else:
                     print("hit wall")
-                    print("mark the point red")
+                    t.penup()
+                    t.done()
+
             # ###########################################
     def characters(self,content):
         self._content = content
