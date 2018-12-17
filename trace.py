26e7e3e9e043bce38018d5e1d35df0ae77b144da
Neal <kb5706@gmail.com>
2018-12-17 17:15:41 +0800

    完成版本。

--- trace.py
+++ trace.py
@@ -14,9 +14,10 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
         if name == "trace":
             print("begin trace")
             print("设定初始值")          #初始化绘制基本参数
-            t = turtle.Pen()
-            t.pensize(5)
-            t.pencolor("blue")
+            turtle.screensize(3000, 3000, "white")
+            turtle.Pen()
+            turtle.pensize(5)
+            turtle.pencolor("blue")
         if name == "beginspot":
             print("begin beginspot")
         if name == "step":
@@ -25,7 +26,7 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
     def endElement(self,name):          #读取结束标签和标签内的内容
         if name == "trace":
             print("end trace")
-            t.done()
+            turtle.done()
         if name == "beginspot":                     #读取起始坐标
             print("beginspot is "+self._content)
             coordinate = self._content.split(",")
@@ -34,107 +35,136 @@ class MoveHandler(sax.ContentHandler): #继承ContentHandler类
             self.y = int(coordinate[1])
             print("x is " + str(self.x))
             print("y is " + str(self.y))
-            t.penup()
-            t.goto(self.x, self.y)
-            t.pencolor("green")
-            t.pendown()
-            if self.x >= 0 and self.x <=30 and self.y >= 0 and self.y <= 30:    #判断起始坐标是否在30*30范围
+            turtle.penup()
+            turtle.goto(self.x, self.y)
+            turtle.pencolor("green")
+            turtle.pendown()
+            if self.x >= 0 and self.x <=3000 and self.y >= 0 and self.y <= 3000:    #判断起始坐标是否在3000*3000范围
                 print("ready to trace")
             else:
-                print("请设定起始坐标在30*30的范围内")###########如果不在范围内程序可能仍会运行#
+                print("请设定起始坐标在3000*3000的范围内")
         if name == "step":                                  #判断移动方向，并按照相应方向进行坐标计算
-            print("step is "+self._content)                 #超出30*30就会撞墙
-            if self._content == "up":
-                self.y = self.y +1
-                if self.y >= 0 and self.y <= 30:
+            print("step is "+self._content)                 #超出3000*3000就会撞墙
+            if self.x >= 0 and self.x <= 3000 and self.y >= 0 and self.y <= 3000:
+                if self._content == "up":
+                    self.y = self.y +100
+                    #if self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x,self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.y > 3000:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x,self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "down":
-                self.y = self.y - 1
-                if self.y >= 0 and self.y <= 30:
+                if self._content == "down":
+                    self.y = self.y - 100
+                    #if self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.y < 0:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "left":
-                self.x = self.x -1
-                if self.x >= 0 and self.x <= 30:
+                if self._content == "left":
+                    self.x = self.x -100
+                    #if self.x >= 0 and self.x <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.x< 0:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "right":
-                self.x = self.x +1
-                if self.x >= 0 and self.x <= 30:
+                if self._content == "right":
+                    self.x = self.x +100
+                    #if self.x >= 0 and self.x <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.x>3000:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "leftup":
-                self.x = self.x -1
-                self.y = self.y +1
-                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                if self._content == "leftup":
+                    self.x = self.x -100
+                    self.y = self.y +100
+                    #if self.x >= 0 and self.x <= 3000 and self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.x<0 or self.y >3000:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "leftdown":
-                self.y = self.y -1
-                self.x = self.x -1
-                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                if self._content == "leftdown":
+                    self.y = self.y -100
+                    self.x = self.x -100
+                    #if self.x >= 0 and self.x <= 3000 and self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.x<0 or self.y <0:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "rightup":
-                self.y = self.y +1
-                self.x = self.x +1
-                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                if self._content == "rightup":
+                    self.y = self.y +100
+                    self.x = self.x +100
+                    #if self.x >= 0 and self.x <= 3000 and self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.x>3000 or self.y >3000:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
 
-            if self._content == "rightdown":
-                self.y = self.y -1
-                self.x = self.x +1
-                if self.x >= 0 and self.x <= 30 and self.y >= 0 and self.y <= 30:
+                if self._content == "rightdown":
+                    self.y = self.y -100
+                    self.x = self.x +100
+                    #if self.x >= 0 and self.x <= 3000 and self.y >= 0 and self.y <= 3000:
                     print("x is " + str(self.x))
                     print("y is " + str(self.y))
-                    t.goto(self.x, self.y)
-                else:
-                    print("hit wall")
-                    t.penup()
-                    t.done()
+                    if self.y<0 or self.x >3000:
+                        print("hit wall! cannot move like this!")
+                    else:
+                        turtle.goto(self.x, self.y)
+                    # else:
+                    #     print("hit wall")
+                    #     turtle.penup()
+                    #     turtle.done()
+            else:
+                print("hit wall! cannot move like this!")
+                turtle.penup()
+                turtle.done()
 
             # ###########################################
     def characters(self,content):
@@ -144,3 +174,4 @@ if __name__ == "__main__":
     handler = MoveHandler()
     #sax.parse("eightDirctions.xml",handler)
     sax.parse("testHitWall.xml", handler)
+    #sax.parse("testAllDict.xml",handler)
